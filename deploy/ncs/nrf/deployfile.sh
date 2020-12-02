f58a8a1a52fafc9fd3b2746e8fa6ddefb1beda81
Makarski, Czeslaw <Czeslaw.Makarski@nordicsemi.no>
2020-12-02 09:37:04 +0100

    KRKNWK-8310: Get rid of deployfiles which are already upstreamed to the master of NCS

--- deploy/ncs/nrf/deployfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "${SOURCE_ROOT_DIR:?}" > /dev/null
-
-if ! grep "802154_rpmsg" "${DEPLOY_TARGET_CURR_DIR}/samples/CMakeLists.txt" ; then
-    patch -d "${DEPLOY_TARGET_CURR_DIR}" -p1 < "${SOURCE_ROOT_DIR}/deploy/ncs/nrf/child-image.patch"
-fi
--- deploy/ncs/zephyr/modules/deployfile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Please note that it may be necessary that file ${DEPLOY_TARGET_CURR_DIR}/Kconfig.nordic
-# is patched to enable new features or configuration options implemented in
-# ncs/modules/hal/nordic
