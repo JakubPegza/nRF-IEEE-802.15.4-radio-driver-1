4bd22013934c032fc1b1b1543365e3839c23ef42
Hubert Miś <hubert.mis@nordicsemi.no>
2020-12-16 09:47:21 +0100

    KRKNWK-8437: Add missing '/' in deployfile
    Without '/' bash script could not correctly find directory to check if
    patch is already applied.

--- deploy/ncs/zephyr/deployfile.sh
+++ deploy/ncs/zephyr/deployfile.sh
@@ -2,6 +2,6 @@
 
 echo "${SOURCE_ROOT_DIR:?}" > /dev/null
 
-if [ ! -d "${DEPLOY_TARGET_CURR_DIR}samples/boards/nrf/ieee802154/802154_rpmsg" ] ; then
+if [ ! -d "${DEPLOY_TARGET_CURR_DIR}/samples/boards/nrf/ieee802154/802154_rpmsg" ] ; then
     patch -d "${DEPLOY_TARGET_CURR_DIR}" -p1 < "${SOURCE_ROOT_DIR}/deploy/ncs/zephyr/serialization-sample.patch"
 fi
