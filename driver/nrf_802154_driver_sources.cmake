65da851a295f1b8ce881c54425b244862fa203bb
Pegza, Jakub <Jakub.Pegza@nordicsemi.no>
2021-02-18 13:54:15 +0100

    Fix filelists

--- driver/nrf_802154_driver_sources.cmake
+++ driver/nrf_802154_driver_sources.cmake
@@ -25,8 +25,7 @@ set(NRF_802154_DRIVER_SOURCES_COMMON
   ${NRF_802154_DRIVER_ROOT}/src/mac_features/ack_generator/nrf_802154_ack_generator.c
   ${NRF_802154_DRIVER_ROOT}/src/mac_features/ack_generator/nrf_802154_enh_ack_generator.c
   ${NRF_802154_DRIVER_ROOT}/src/mac_features/ack_generator/nrf_802154_imm_ack_generator.c
-  ${NRF_802154_DRIVER_ROOT}/src/platform/random/nrf_802154_random_zephyr.c
-  ${NRF_802154_DRIVER_ROOT}/src/platform/temperature/nrf_802154_temperature_none.c
+  ${NRF_802154_DRIVER_ROOT}/platform/temperature/nrf_802154_temperature_none.c
   )
 
 set(NRF_802154_DRIVER_SOURCES_NRF52
